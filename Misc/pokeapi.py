0a47c61081358a2926b498dd01b80d815a63fac3
Chase Peterson <belax_8@hotmail.com>
2016-10-29 14:10:56 -0600

    Added Pokemon api.

---
+++ Misc/pokeapi.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+
+# Imports
+import requests
+from sys import version_info
+
+
+# Setup
+py3 = version_info[0] > 2
+
+if py3:
+	pokemon = input('Which pokemon would you like to look up? ')
+else:
+	pokemon = raw_input('Which pokemon would you like to loop up? ')
+
+
+# Make Request
+url = 'http://pokeapi.co/api/v2/pokemon/' + pokemon
+req = requests.get(url)
+data = req.json()
+
+
+# Display data
+if req.status_code != 200:
+	print('')
+	print('Sorry that pokemon doesn\'t exist')
+	print('')
+else:
+	print('')
+	print('Name: ' + data['name'].title())
+	print('Height: ' + str(data['height']))
+	print('Weight: ' + str(data['weight']))
+	print('Base Experience: ' + str(data['base_experience']))
+
+	print('')
+	print('Abilities:')
+	for x in data['abilities']:
+		print(' * ' + x['ability']['name'])
+
+	print('')
+	print('Held Items:')
+	for n in data['held_items']:
+		print(' * ' + n['item']['name'])
+
+	print('')
